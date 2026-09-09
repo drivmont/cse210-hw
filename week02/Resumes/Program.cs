7f825d28d17e27a03008063ac8e488361d5c4cd6
drivmont <rivmont.diego@gmail.com>
2026-09-09 15:42:32 -0500

    Week 02 Resume

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";  
+        job1._startYear = 2015;
+        job1._endYear = 2020;
+        Job job2 = new Job();
+        job2._company = "Google";  
+        job2._jobTitle = "Senior Developer";
+        job2._startYear = 2020;
+        job2._endYear = 2023;
+        Resume resume = new Resume();
+        Console.Write("Name:");
+        resume._name = Console.ReadLine();
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+        resume.Display();
     }
 }
